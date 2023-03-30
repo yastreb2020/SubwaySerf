348aaa69f1d38f51ec0549b23752f09b99f04fb1
Katya Yastrebkova <katya2014ya@gmail.com>
2023-03-30 14:17:17 +0300

    basic player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+
+    private float moveStep = 2.5f;
+    public float jumpSpeed;
+
+    /// <summary>
+    /// 1 - left, 2 - middle, 3 - right
+    /// </summary>
+    private int currentLane;
+
+    private bool isOnGround = true;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        currentLane = 2;
+    }
+
+    void Update()
+    {
+        ChangeLanes();
+
+        // jump
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isOnGround){
+            playerRb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            isOnGround = false;
+        }
+
+        // float horizontalInput = Input.GetAxis("Horizontal");
+        // transform.Translate(Vector3.right * horizontalInput * moveStep);
+    }
+
+    private void ChangeLanes()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && currentLane > 1)
+        {
+            transform.position -= new Vector3(moveStep, 0, 0);
+            currentLane--;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && currentLane < 3)
+        {
+            transform.position += new Vector3(moveStep, 0, 0);
+            currentLane++;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# SubwaySerf game
